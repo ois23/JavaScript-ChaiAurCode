4fb703914f6e539de2a39ef434ba082ed3895c71
Avais <127426190+ois23@users.noreply.github.com>
2023-07-25 15:44:44 +0000

    Datatype part 1

---
+++ 01_Basics/02_Datatype.js
@@ -0,0 +1,23 @@
+// "use strict";   // treat all JS code newer version
+
+// alert(2+9)       // we are using nodejs, not browser
+
+console.log(6+9)
+console.log("Avais")
+
+
+let name = "avais"
+let age = 39
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint => big intiger for log value
+// string => ""
+// boolean => true/false
+// null => standalone value or empty, but not zero
+// undefined => value not assined 
+// symbol => 
+// object => 
+
+console.log (typeof age);   // to know the type of variable 
