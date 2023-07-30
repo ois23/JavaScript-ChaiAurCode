77d05c79e242c0fed2174bf6353bbe71bd34acab
Avais <127426190+ois23@users.noreply.github.com>
2023-07-30 16:26:27 +0000

    Numbers done

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,28 @@
+// const score = 400
+// console.log (score);
+
+// const balance = new Number(100)
+// console.log(balance);                       //==> [Number: 100]
+
+// console.log(balance.toString().length);     // ==> 3 , show the variable length , length of 100 is 3
+// // console.log(balance.toFixed());             // => 100, fixed the digit of fractional values
+// console.log(balance.toFixed(2));             // => 100.00, fixed the digit
+
+
+// const othrNumber = 123.456789
+
+// console.log(othrNumber.toPrecision(5));     // => 123.46 , take precise value / round up the value at given number
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString());           // ==> 10,000,000 , used to add commas , default  in US number system
+// console.log(hundreds.toLocaleString('en-IN'));          // 1,00,00,000  , en-IN is used to make in US to Indian system
+
+
+
+// ++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++
+// maths lab is default added in JS
+
+
+
+
+
